da71754ddec9af7043706aa7558e194da8f0745e
VnomArrow <VnomArrow@gmail.com>
2020-09-17 09:17:49 +0200

    setup simple visual studio community project

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+	// Print "hello world!"
+	std::cout << "hello world!" << std::endl;
+
+	// To not close the window immediately
+	std::cin.get();
+
+	return 0;
+}
