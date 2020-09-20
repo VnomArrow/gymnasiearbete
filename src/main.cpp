93ff9df318d3b60b86ee41269892d66b6712a705
VnomArrow <VnomArrow@gmail.com>
2020-09-20 13:12:42 +0200

    fix simulation demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include "mandelbrot_generator.h"
+#include "simulator/Simulator.h"
+#include "simulator/Vec4.h"
 
 int wmain() {
-	mandelbrot_main();
+	Vec4 vec = Vec4(3, 3, 0, 0);
+	Simulator simulator = Simulator(vec, 100, 100, 100, 100, 10);
+	DisplayWindow::init(800, 800);
+	while (DisplayWindow::isRunning()) {
+		simulator.iterateSnapshot(0.01);
+		DisplayWindow::fill(0, 0, 0, 255);
+		DisplayWindow::drawRectangle(400, 400, 20, 20, 0, 0, 255, 255);
+		DisplayWindow::drawRectangle(400 - simulator.getx1(), 400 - simulator.gety1(), 20, 20, 255, 0, 0, 255);
+		DisplayWindow::drawRectangle(400 - simulator.getx2(), 400 - simulator.gety2(), 20, 20, 0, 255, 0, 255);
+		DisplayWindow::flip();
+		DisplayWindow::update();
+	}
+
+
+	//mandelbrot_main();
 	// Print "hello world!"
 	std::cout << "hello world!" << std::endl;
 
